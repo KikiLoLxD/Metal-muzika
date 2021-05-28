3aa00e503464c86bf4a06667cb78d31862fbebee
KikiLoLxD <84983409+KikiLoLxD@users.noreply.github.com>
2021-05-28 23:14:37 +0200

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,40 @@
+function muzika() {
+  var x = document.getElementById("muzika");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+function izvori() {
+  var x = document.getElementById("izvori");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+function autor() {
+  var x = document.getElementById("autor");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+function sajt() {
+  var x = document.getElementById("sajt");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+function pesme() {
+  var x = document.getElementById("pesme");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
